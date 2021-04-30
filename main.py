6469f5ce235ab600acbb0eb5c64a7de48298ff3b
alicevillar <av20678@essx.ac.uk>
2021-04-30 11:35:29 -0300

    form filling

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ from selenium.webdriver.support import expected_conditions as eco
 from selenium.webdriver.common.by import By
 from faker import Faker
 
+# automatizacao de preenchimento de formulario
 
-#automatizacao de preenchimento de formulario
+################################################################
+# Part 1: Initializing
+#   1.1 Configuring the driver
+#   1.2 Accessing the webpage using method get usando get
+#   1.3 Referencing search box
+#   1.4. Filling all buttons
+###############################################################
 
-#####################################
-# Passo 1: Configurar o driver
-#
-####################################
-
+# Part 1.1 Configuring the driver
 PATH = "C://Drivers/chromedriver.exe"
-driver=webdriver.Chrome(PATH)
+driver = webdriver.Chrome(PATH)
 
-#####################################
-# Passo 2:
-# Acessar a pagina usando get
-# Referenciar caixa de pesquisa
-####################################
-
+# Part 1.2 Accessing the webpage using method get usando get
 driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account")
 
-search_input=driver.find_element_by_xpath('//*[@id="email_create"]')
+# Part 1.3 Referencing search box
+search_input = driver.find_element_by_xpath('//*[@id="email_create"]')
 
 # Using the method send_keys, which is aimed to write in the search box
 search_input.send_keys("Jennifer@uol.com.br")
@@ -34,53 +33,90 @@ search_input.send_keys("Jennifer@uol.com.br")
 # Using usando method send_keys to press ENTER
 search_input.send_keys(Keys.ENTER)
 
-#PREENCHENDO RADIO BUTTON
+# Part 1.4 Configuring the driver
 
-# localiza elemento pelo id e clica, mas não funciona justamente pq o formulário carrega num tempo diferente da página. Então, quando mando clicar na página, o formulário ainda não tá carregado
-#driver.find_element_by_id("id_gender2").click()
+# Filling Radio Button
 
-# Waiting one second for the web browser to open the page
-#Fixando o tempo de 30 segundos até que o radio button esteja pronto
-#det que o driver vai esperar 30 segundo. det q seja até que o elemento abra. Encontro o elemento usando a classe By e o método ID dessa classe
-# O locator é o - By.ID,"id_gender2" - , que é o parâmetro do método presence_of_element_located.
-# presence_of_element_located returns WebElement once it is located
-# A classe By dá uma estratégia para definir o parâmetro locator (ID é uma das estratégias que a classe By oferece)
-
-element_radio_gender=WebDriverWait(driver,30).until(eco.presence_of_element_located((By.ID,"id_gender2")))
-
-#o elemento foi encontrado e foi salvo nessa variável, então eu clico nela
+# First, give some time for web browser to open the page
+element_radio_gender = WebDriverWait(driver, 30).until(eco.presence_of_element_located((By.ID, "id_gender2")))
+# Found element is in this variable, so I'll click
 element_radio_gender.click()
 
-#preenchendo o box first name
-#como não preciso clicar enter nao preciso criar uma variável. Posso fazer a sintae toda junta :)
+# Box - first name
+# como não preciso clicar enter nao preciso criar uma variável. Posso fazer a sintae toda junta :)
 driver.find_element_by_xpath('//*[@id="customer_firstname"]').send_keys("Jennifer")
 
-#preenchendo o box last name
+# preenchendo o box last name
 driver.find_element_by_xpath('//*[@id="customer_lastname"]').send_keys("Jobs")
 
-#preenchendo o box password
+# preenchendo o box password
 driver.find_element_by_xpath('//*[@id="passwd"]').send_keys("Ganesha!")
 
-#preenchendo o box date of birth - box day (drop down)
-select_day=Select(driver.find_element_by_id("days"))
+# Box - date of birth (drop down)
+select_day = Select(driver.find_element_by_id("days"))
 # Selecting by value
 select_day.select_by_value("4")
 
-#preenchendo o box date of birth - box month (drop down)
+# preenchendo o box date of birth - box month (drop down)
 
-select_month=Select(driver.find_element_by_id("months"))
+select_month = Select(driver.find_element_by_id("months"))
 select_month.select_by_value("1")
 
-#preenchendo o box date of birth - box year (drop down)
-select_year=Select(driver.find_element_by_id("years"))
+# Box - date of birth - box year (drop down)
+select_year = Select(driver.find_element_by_id("years"))
 select_year.select_by_value("1992")
 
-#preenchendo check box
+# Check box
 driver.find_element_by_id("newsletter").click()
 driver.find_element_by_id("optin").click()
 
+# preenchendo Adress
+# box - first name
+driver.find_element_by_xpath('//*[@id="firstname"]')
 
+# box - last name
+driver.find_element_by_xpath('//*[@id="lastname"]')
 
+# box - company
+driver.find_element_by_xpath('//*[@id="company"]').send_keys("If Boutique Inc")
 
+# Box - Address *
 
+# Tyger City Shopping Centre
+driver.find_element_by_xpath('//*[@id="address1"]').send_keys("T94 Grand St.")
 
+# Box - Address 2
+driver.find_element_by_xpath('//*[@id="address2"]').send_keys("NY 10013")
+
+# Box - City
+driver.find_element_by_xpath('//*[@id="city"]').send_keys("New York")
+
+# Box - State
+
+select_year = Select(driver.find_element_by_id("id_state"))
+select_year.select_by_value("32")
+
+# Box - Zip/Postal Code
+driver.find_element_by_xpath('//*[@id="postcode"]').send_keys("64646")
+
+# Box - Country
+select_year = Select(driver.find_element_by_id("id_country"))
+select_year.select_by_value("21")
+
+# Box - Additional information
+driver.find_element_by_xpath('//*[@id="other"]').send_keys("No additional information")
+
+# Box - Home Phone
+driver.find_element_by_xpath('//*[@id="phone"]').send_keys("33445566")
+
+# Box - Mobile Phone
+driver.find_element_by_xpath('//*[@id="phone_mobile"]').send_keys("992828282")
+
+# Box - Assign an address alias for future reference
+driver.find_element_by_xpath('//*[@id="alias"]').send_keys("170 Ludlow St")
+
+# Box - Assign an address alias for future reference
+
+# Box - Register: Using method send_keys to press ENTER
+search_input = driver.find_element_by_xpath('//*[@id="submitAccount"]')
+search_input.send_keys(Keys.ENTER)
